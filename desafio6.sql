ac5cab5a38de9ea6d970bc516f9fc388a2aa0c0e
Sidnei Ramos <sidneiramos02@gmail.com>
2020-10-03 16:33:08 -0300

    CC

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(em.FIRST_NAME,' ',em.LAST_NAME) AS 'Nome completo',
- jo.JOB_TITLE AS 'Cargo',
- jh.START_DATE AS 'Data de início do cargo',
- de.DEPARTMENT_NAME AS 'Departamento'
+jo.JOB_TITLE AS 'Cargo',
+jh.START_DATE AS 'Data de início do cargo',
+de.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.job_history AS jh
 INNER JOIN hr.employees AS em
 ON jh.EMPLOYEE_ID = em.EMPLOYEE_ID
