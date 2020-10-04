c0b4e12fc405c0c3c81ec38d0048057a9848970a
Sidnei Ramos <sidneiramos02@gmail.com>
2020-10-03 22:08:37 -0300

    Finalmente esse desafio 12 resolvido

---
+++ desafio11.sql
@@ -0,0 +1,6 @@
+SELECT p.ContactName AS 'Nome',
+p.Country AS 'País',
+COUNT(t.Country) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS p, w3schools.customers AS t
+WHERE p.Country = t.Country
+GROUP BY 1;
