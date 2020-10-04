b4a18a8231ff446e5b01f46718a17ef233459acb
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-04 19:18:13 -0300

    ...

---
+++ desafio6.sql
@@ -0,0 +1,28 @@
+-- Desafio 6
+-- Faça um relatório que mostra o histórico de cargos das pessoas empregadas. Para isso, usando o banco hr 
+-- como referência, monte uma query que exiba quatro colunas:
+-- 
+-- A primeira coluna deve possuir o alias "Nome completo" e exibir o nome completo da pessoa empregada 
+-- (não se esqueça do espaço entre o nome e o sobrenome).
+-- 
+-- A segunda coluna deve possuir o alias "Cargo" e exibir o nome do cargo da pessoa.
+-- 
+-- A terceira coluna deve possuir o alias "Data de início do cargo" e exibir a data que a pessoa iniciou o cargo.
+-- 
+-- A quarta coluna deve possuir o alias "Departamento" e mostrar o nome do departamento em que a pessoa
+--  exerceu seu cargo.
+-- 
+-- Os resultados devem estar ordenados pelo nome completo das pessoas empregadas em ordem decrescente. 
+-- Em caso de empate no nome completo, 
+-- ordene os resultados pelo nome do cargo em ordem alfabética.
+
+SELECT
+CONCAT(e.FIRST_NAME,' ', e.LAST_NAME) AS `Nome completo`,
+j.JOB_TITLE AS Cargo,
+h.START_DATE AS 'Data de início do cargo',
+d.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.job_history AS h
+INNER JOIN hr.jobs AS j ON h.JOB_ID = j.JOB_ID
+INNER JOIN hr.employees AS e ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.departments AS d ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo;
