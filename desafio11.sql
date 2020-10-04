9be0a833023f197bbd9b1332e00456e3c1a200ca
Sidnei Ramos <sidneiramos02@gmail.com>
2020-10-03 22:17:22 -0300

    Esse desafio 11 que nao quer passar

--- desafio11.sql
+++ desafio11.sql
@@ -3,4 +3,5 @@ p.Country AS 'País',
 COUNT(t.Country) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS p, w3schools.customers AS t
 WHERE p.Country = t.Country
-GROUP BY 1;
+GROUP BY 1
+ORDER BY 1;
