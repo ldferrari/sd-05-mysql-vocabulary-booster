1919192f7fb7a9a3387a4be60f54e6df51a75d8b
Sidnei Ramos <sidneiramos02@gmail.com>
2020-10-03 23:02:03 -0300

    Acho que agora esse 11 passa

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,9 @@
 SELECT p.ContactName AS 'Nome',
 p.Country AS 'País',
-COUNT(t.Country) - 1 AS 'Número de compatriotas'
-FROM w3schools.customers AS p, w3schools.customers AS t
-WHERE p.Country = t.Country
-GROUP BY 1
-ORDER BY 1;
+(
+SELECT COUNT(t.Country)
+FROM w3schools.customers AS t
+WHERE t.Country = p.Country
+) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS p
+ORDER BY 1; 
