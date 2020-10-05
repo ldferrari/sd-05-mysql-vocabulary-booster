a6e504c9fffac062b2f6453c9d8b5ab9f19e8f01
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-05 16:36:58 -0300

    cc

--- desafio9.sql
+++ desafio9.sql
@@ -4,5 +4,4 @@ COUNT(*) AS `Total de pedidos`
 FROM w3schools.orders AS o
 INNER JOIN w3schools.employees AS em ON o.EmployeeID = em.EmployeeID
 GROUP BY `Nome completo`
-ORDER BY `Total de pedidos`
-;
+ORDER BY `Total de pedidos`;
