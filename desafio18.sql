ac014ff9fe74a572bab6ac2e032d2eac0591d231
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-05 18:47:17 -0300

    desafio 10 done and empty files of the next others

---
+++ desafio18.sql
@@ -0,0 +1,4 @@
+-- Desafio 18
+-- Crie uma função chamada exibir_quantidade_pessoas_contratadas_por_mes_e_ano no banco de dados hr que, dados o mês e ano como parâmetros nessa ordem, 
+-- retorna a quantidade de pessoas funcionárias que foram contratadas nesse mês e ano. 
+-- Confirme que sua função retorna o valor 14 ao ser chamada passando os números 6 e 1987 para mês e ano, respectivamente.
