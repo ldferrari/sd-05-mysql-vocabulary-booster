2c1fa7c2ae6da43dc9ae9dd8d8fde8357e4a45cb
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-05 16:34:39 -0300

    desafio9

---
+++ desafio9.sql
@@ -0,0 +1,8 @@
+SELECT
+CONCAT(FirstName, ' ', LastName) AS `Nome completo`,
+COUNT(*) AS `Total de pedidos`
+FROM w3schools.orders AS o
+INNER JOIN w3schools.employees AS em ON o.EmployeeID = em.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`
+;
