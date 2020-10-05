c0bccd2b76f92d556ec3eb9463a4b3ee8e3083ec
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-10-05 12:46:20 -0300

    Tô achando que estou no vscode - comentários sem TAB sempre!

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
 -- Desafio 2 A primeira coluna deve possuir o alias "Cargo" e exibir o nome do cargo.
 -- A segunda coluna deve possuir o alias "Nível" e exibir o nível de remuneração do cargo com base no seu salário máximo, categorizando os níveis de remuneração da seguinte forma:
-	-- Salário máximo entre 5000 e 10000 -> Baixo
-	-- Salário máximo entre 10001 e 20000 -> Médio
-	-- Salário máximo entre 20001 e 30000 -> Alto
-	-- Salário máximo acima de 30000 -> Altíssimo
+-- Salário máximo entre 5000 e 10000 -> Baixo
+-- Salário máximo entre 10001 e 20000 -> Médio
+-- Salário máximo entre 20001 e 30000 -> Alto
+-- Salário máximo acima de 30000 -> Altíssimo
 -- Os resultados devem estar ordenados pelo nome do cargo em ordem alfabética.
 -- Selecionar a coluna JOB_TITLE e renomear para Cargo, junto com o select usar o case e especificar condições e valores
 -- assim que finaliza - resultado impresso na coluna com nome Nível por último ordena os resultados em ordem crescente
@@ -17,4 +17,3 @@ WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
 END AS 'Nível'
 FROM hr.jobs
 ORDER BY Cargo;
-
