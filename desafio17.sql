280dc540d7d9966611ee4e47efaeea45b03fb7c1
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-06 15:34:01 -0300

    y to Y on dateformat()

--- desafio17.sql
+++ desafio17.sql
@@ -17,8 +17,8 @@
 -- por ela no cargo em questão, em ordem crescente.
 SELECT
 CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
-DATE_FORMAT(START_DATE,'%d/%m/%y') AS 'Data de início',
-DATE_FORMAT(END_DATE,'%d/%m/%y') AS 'Data de rescisão',
+DATE_FORMAT(START_DATE,'%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(END_DATE,'%d/%m/%Y') AS 'Data de rescisão',
 ROUND(TIMESTAMPDIFF(DAY , START_DATE, END_DATE) / 365 , 2) AS `Anos trabalhados`
 FROM hr.job_history AS J, hr.employees AS EM
 WHERE J.EMPLOYEE_ID = EM.EMPLOYEE_ID
