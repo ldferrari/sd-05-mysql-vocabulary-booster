1c222fa35812220c24e3c309736d2f2584b2cab7
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-10-06 18:31:35 -0300

    desafio 3 feito

--- desafio3.sql
+++ desafio3.sql
@@ -1,1 +1,2 @@
-SELECT JOB_TITLE as 'Cargo', (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo' FROM hr.jobs;
+SELECT JOB_TITLE as 'Cargo', (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+FROM hr.jobs ORDER BY 2, 1;
