4daace379b8a328f6db623d79367bf6bc5b41edf
Rafael Assad <rafaassadeng@gmail.com>
2020-10-06 15:46:18 -0300

    6

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+SELECT CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
+job.JOB_TITLE AS Cargo,
+jh.START_DATE AS 'Data de início do cargo',
+dep.DEPARTMENT_NAME AS Departamento
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS emp
+ON jh.EMPLOYEE_ID = emp.EMPLOYEE_ID
+INNER JOIN hr.jobs AS job
+ON jh.JOB_ID = job.JOB_ID
+INNER JOIN hr.departments AS dep
+ON emp.DEPARTMENT_ID = dep.DEPARTMENT_ID
+ORDER BY 1 DESC, 2;
