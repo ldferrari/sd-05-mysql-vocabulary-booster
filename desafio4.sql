33cd87714fc45620d0034f8159708303ae37588b
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-10-05 22:17:17 -0300

    Passando no desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT jbs.JOB_TITLE AS 'Cargo',
+  ROUND(AVG(emp.SALARY), 2) AS 'Média salarial',
+  CASE
+    WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN ROUND(AVG(emp.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    WHEN ROUND(AVG(emp.SALARY), 2) > 10500 THEN 'CEO'
+  END AS 'Senioridade'
+FROM hr.employees AS emp
+  INNER JOIN hr.jobs AS jbs ON (emp.JOB_ID = jbs.JOB_ID)
+GROUP BY jbs.JOB_TITLE
+ORDER BY ROUND(AVG(emp.SALARY), 2),
+  jbs.JOB_TITLE;
