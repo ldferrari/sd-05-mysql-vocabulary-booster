aabbe4661a29e86c31ded900d9f7d7f9e669fa2c
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-06 10:13:51 -0300

    desafio12, apparently working

--- desafio12.sql
+++ desafio12.sql
@@ -27,5 +27,19 @@
 -- 
 -- Os resultados devem estar ordenados pela coluna "Nome completo funcionário 1" em ordem alfabética. Em caso de empate, os resultados devem ser ordenados pela coluna 
 -- Nome completo funcionário 2" em ordem alfabética.
--- 
--- 
+select * from hr.employees;
+SELECT
+CONCAT(e1.FIRST_NAME, ' ', e1.LAST_NAME) AS `Nome completo funcionário 1`,
+e1.SALARY AS 'Salário funcionário 1',
+e1.PHONE_NUMBER AS 'Telefone funcionário 1',
+CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME) AS `Nome completo funcionário 2`,
+e2.SALARY AS 'Salário funcionário 2',
+e2.PHONE_NUMBER AS 'Telefone funcionário 2'
+FROM hr.employees as e1, hr.employees as e2
+WHERE e1.JOB_ID = e2.JOB_ID AND e1.EMPLOYEE_ID <> e2.EMPLOYEE_ID
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
+
+
+-- CONCAT(e1.FIRST_NAME, ' ', e1.LAST_NAME) AS `Nome completo funcionário 
+-- e1.SALARY AS 'Salário funcionário 1',
+-- e1.PHONE_NUMBER AS 'Telefone funcionário 1'
