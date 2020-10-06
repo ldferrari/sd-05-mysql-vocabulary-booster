7b163100a3b3a1c0fd1851d7a2831d06cb3c3b5c
rfreitasbatista <rfreitasbatista@gmail.com>
2020-10-06 16:13:55 -0300

    Issues correction

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT
 COUNTRY_NAME 'País', 
-IF (REGION_ID = 1 'incluído', 'não incluído') `Status Inclusão`
+IF (REGION_ID = 1, 'incluído', 'não incluído') `Status Inclusão`
 FROM hr.countries
 ORDER BY COUNTRY_NAME;
