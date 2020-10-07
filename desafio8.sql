7441881090ab212ec214909d9c1b854e323fe1de
Rafael Assad <rafaassadeng@gmail.com>
2020-10-07 08:24:05 -0300

    bora ler mais

--- desafio8.sql
+++ desafio8.sql
@@ -1,4 +1,4 @@
-SELECT cus.CustomerName AS "Nome de contato",
+SELECT cus.ContactName AS "Nome de contato",
 ship.ShipperName AS "Empresa que fez o envio",
 ord.OrderDate AS "Data do pedido"
 FROM w3schools.customers AS cus
@@ -7,4 +7,4 @@ ON cus.CustomerID = ord.CustomerID
 INNER JOIN w3schools.shippers AS ship
 ON ship.ShipperID = ord.ShipperID
 WHERE ship.ShipperName IN('Speedy Express', 'United Package')
-ORDER BY 1, 2, 3 ASC;
+ORDER BY 1, 2, 3;
