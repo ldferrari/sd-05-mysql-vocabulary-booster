9f7472152e9166deb7d8b6d6a0fb3ba5ef92799f
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-07 17:46:17 -0300

    desafio 19 working but not tested yet

--- desafio16.sql
+++ desafio16.sql
@@ -1,7 +1,3 @@
--- Desafio 16
--- Crie uma função chamada buscar_quantidade_de_empregos_por_funcionario no banco de dados hr que, ao receber o email de uma pessoa funcionária,
---  retorne a quantidade de empregos presentes em seu histórico. Confirme que sua função retorna o valor 2 ao ser
---  chamada passando uma pessoa funcionária cujo email é "NKOCHHAR".
 DROP FUNCTION IF EXISTS buscar_quantidade_de_empregos_por_funcionario;
 DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(EM VARCHAR(50))
@@ -13,7 +9,7 @@ COUNT(*)
 FROM
 hr.employees as em, hr.job_history as h
 WHERE em.EMPLOYEE_ID = h.EMPLOYEE_ID AND
-EMAIL LIKE 'NKOCHHAR%' INTO quantidade_de_jobs;
+EMAIL = CONCAT(EM, '@example.com') INTO quantidade_de_jobs;
 RETURN quantidade_de_jobs;
 END $$
 
--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,3 @@
--- Desafio 18
--- Crie uma função chamada exibir_quantidade_pessoas_contratadas_por_mes_e_ano no banco de dados hr que, dados o mês e ano como parâmetros nessa ordem, 
--- retorna a quantidade de pessoas funcionárias que foram contratadas nesse mês e ano. 
--- Confirme que sua função retorna o valor 14 ao ser chamada passando os números 6 e 1987 para mês e ano, respectivamente.
 DROP FUNCTION IF EXISTS exibir_quantidade_pessoas_contratadas_por_mes_e_ano;
 DELIMITER $$
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
@@ -18,22 +14,3 @@ END $$
 DELIMITER ;
  
 SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6, 1987) AS total;
--- -- 
--- DROP FUNCTION IF EXISTS buscar_quantidade_de_empregos_por_funcionario;
--- DELIMITER $$
--- CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(EM VARCHAR(50))
--- RETURNS INT READS SQL DATA
--- BEGIN
--- DECLARE quantidade_de_jobs INT;
--- SELECT
--- COUNT(*)
--- FROM
--- hr.employees as em, hr.job_history as h
--- WHERE em.EMPLOYEE_ID = h.EMPLOYEE_ID AND
--- EMAIL LIKE 'NKOCHHAR%' INTO quantidade_de_jobs;
--- RETURN quantidade_de_jobs;
--- END $$
--- 
--- DELIMITER ;
--- 
--- SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR') AS 'total_empregos';
