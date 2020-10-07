b4f1c0ec04b4e435f3d9f91c43dd920b992ff3a6
Lucas Costa Ventura <lucascv333@gmail.com>
2020-10-07 12:55:45 -0300

    incluindo ON dentro do JOIN

--- desafio7.sql
+++ desafio7.sql
@@ -4,4 +4,5 @@ JH.START_DATE `Data de início`,
 E.SALARY `Salário`
 FROM hr.employees as E
 INNER JOIN hr.job_history as JH
+ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
 ORDER BY `Nome completo`, DATE(`Data de início`);
