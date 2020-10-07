a2f988f813b132a5d2ee1e65d84cae3cd7ab69b2
Lucas Costa Ventura <lucascv333@gmail.com>
2020-10-07 13:30:51 -0300

    c

--- desafio7.sql
+++ desafio7.sql
@@ -2,8 +2,8 @@ SELECT
 UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) `Nome completo`,
 JH.START_DATE `Data de início`,
 E.SALARY `Salário`
-FROM hr.employees as E
-INNER JOIN hr.job_history as JH
+FROM hr.job_history as JH
+INNER JOIN hr.employees as E
 ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
 WHERE MONTH(JH.START_DATE) BETWEEN 1 AND 3
 ORDER BY `Nome completo`, DATE(`Data de início`);
