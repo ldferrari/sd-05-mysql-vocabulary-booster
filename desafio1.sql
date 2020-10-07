4b778e9c53a4863d47ae26ac531acefe15df08b2
hleoc <informig@gmail.com>
2020-10-07 11:16:34 -0300

    Item 1 - 3 verificação

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,2 @@
-SELECT C.country_name AS 'País', IF(R.region_name = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
-FROM hr.regions AS R
-INNER JOIN hr.countries AS C
-ON R.region_id = C.region_id;
+SELECT country_name AS 'País', IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries;
