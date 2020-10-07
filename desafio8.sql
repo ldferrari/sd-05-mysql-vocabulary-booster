b091efa60ee5350d3550496a282cc99ff5f16d77
Lucas Costa Ventura <lucascv333@gmail.com>
2020-10-07 15:54:12 -0300

    add requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT
+C.CustomerName `Nome de contato`,
+S.ShipperName `Empresa que fez o envio`,
+O.OrderDate `Data do pedido`
+FROM w3schools.orders AS O
+INNER JOIN w3schools.customers AS C
+ON O.CustomerID = C.CustomerID
+INNER JOIN w3schools.shippers AS S
+ON S.ShipperID = O.ShipperID
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
