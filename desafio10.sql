fbeb76e730efa7e517855780c121adb46ab80522
Andrevlc <andrevezi@gmail.com>
2020-10-07 17:59:23 -0300

    Requisite 10 second order by fixed

--- desafio10.sql
+++ desafio10.sql
@@ -6,4 +6,4 @@ ROUND(AVG(Quantity),2) AS 'Média'
 FROM order_details
 INNER JOIN products ON products.ProductID = order_details.ProductID
 GROUP BY products.ProductName HAVING ROUND(AVG(Quantity),2) > 20
-ORDER BY `Média`;
+ORDER BY `Média`, `Produto`;
