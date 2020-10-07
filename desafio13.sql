870f9ed192ee0887c6329291f606952a398befb3
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-10-07 17:58:33 -0300

    Desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,12 @@
+-- Exibe todos produtos que já tiveram um pedido associado requerindo uma quantidade 
+-- desse produto maior que 80. Usando o banco w3schools como referência, monte uma query que exiba duas colunas:
+-- A primeira coluna deve possuir o alias "Produto" e exibir o nome do produto.
+-- A segunda coluna deve possuir o alias "Preço" e exibir o preço desse produto.
+-- Os resultados devem estar ordenados pelo nome do produto em ordem alfabética.
+
+SELECT p.ProductName AS 'Produto', Price AS 'Preço'
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS o
+ON p.ProductID = o.ProductID
+WHERE o.Quantity > 80 -- Selecionar a quantidade de produtos maior que 80
+ORDER BY 1;
