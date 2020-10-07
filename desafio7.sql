86cfe339e8c2e2a59aea1b010212f80cff7bdb5a
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-10-07 18:16:47 -0300

    Desafio 7 feito

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT (FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo',
+jh.START_DATE AS 'Data de início do cargo',
+em.SALARY AS 'Salário'
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS em
+ON jh.EMPLOYEE_ID = em.EMPLOYEE_ID
+WHERE MONTH (jh.START_DATE) BETWEEN '01' AND '03'
+ORDER BY 1, 2;
