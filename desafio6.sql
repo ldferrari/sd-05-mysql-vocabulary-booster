ad77ac365dec816f42fa5445a6342f5572cacab1
hleoc <informig@gmail.com>
2020-10-07 18:36:11 -0300

    Item 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,10 @@
-SELECT * FROM employees;
-SELECT * FROM jobs;
-
 SELECT CONCAT(E.first_name,' ', E.last_name) AS 'Nome completo',
 J.job_title AS 'Cargo',
-E.hire_date AS 'Data de início do cargo'
+E.hire_date AS 'Data de início do cargo',
+D.department_name AS 'Departamento'
 FROM hr.employees E
 INNER JOIN hr.jobs AS J
-ON E.job_id = J.job_id;
+ON E.job_id = J.job_id
+INNER JOIN hr.departments AS D
+ON E.department_id = D.department_id
+ORDER BY CONCAT(E.first_name,' ', E.last_name) DESC, J.job_title;
