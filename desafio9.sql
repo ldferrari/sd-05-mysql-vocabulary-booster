df83ffd812468bda5c78e36395a37cf5b11a7c79
pedrohcalado <pedrocalado22@gmail.com>
2020-10-07 15:23:36 -0300

    fix 9

--- desafio9.sql
+++ desafio9.sql
@@ -3,5 +3,5 @@ CONCAT(employees.firstName, ' ', employees.lastName) AS `Nome completo`,
 COUNT(orders.orderID) AS `Total de pedidos`
 FROM w3schools.employees
 JOIN w3schools.orders ON orders.employeeID = employees.employeeID
-GROUP BY employees.firstName
+GROUP BY `Nome completo`
 ORDER BY `Total de pedidos`;
