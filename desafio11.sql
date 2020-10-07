41d705e4578c98377e2aa36ad21babb329d06fdd
pedrohcalado <pedrocalado22@gmail.com>
2020-10-07 16:02:39 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT
+a.contactName AS 'Nome',
+a.country AS 'País',
+COUNT(a.country) - 1 AS `Número de compatriotas`
+FROM w3schools.customers AS a, w3schools.customers AS b
+WHERE a.country = b.country
+GROUP BY a.customerName HAVING `Número de compatriotas` > 0
+ORDER BY a.contactName;
