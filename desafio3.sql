c91074d2d40955d4845a9179155fe29e05381546
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-10-07 13:35:32 -0300

    Desafio 3: done. Obrigada Hugao

--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,6 @@
--- falta ordenar por ordem crescente o salario e
--- se der empate ordenar por nome de cargo
-
 SELECT
 job_title AS "Cargo",
 max_salary - min_salary AS "Diferença entre salários máximo e mínimo"
 FROM hr.jobs
-ORDER BY MAX_SALARY ASC, JOB_TITLE ASC;
+ORDER BY max_salary - min_salary, JOB_TITLE;
+-- Obrigada Hugão, por arrumar minha ordenação kkkk
