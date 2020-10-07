35172f33c9c2b095e2b4dbcf7d66f60f60ad6aad
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-07 16:39:00 -0300

    cc

--- desafio16.sql
+++ desafio16.sql
@@ -7,14 +7,14 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(EM VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE quantidade_de_jobs INT;
-    SELECT
-	COUNT(*)
-	FROM
-	hr.employees as em, hr.job_history as h
-	WHERE em.EMPLOYEE_ID = h.EMPLOYEE_ID AND
-	EMAIL LIKE 'NKOCHHAR%' INTO quantidade_de_jobs;
-    RETURN quantidade_de_jobs;
+DECLARE quantidade_de_jobs INT;
+SELECT
+COUNT(*)
+FROM
+hr.employees as em, hr.job_history as h
+WHERE em.EMPLOYEE_ID = h.EMPLOYEE_ID AND
+EMAIL LIKE 'NKOCHHAR%' INTO quantidade_de_jobs;
+RETURN quantidade_de_jobs;
 END $$
 
 DELIMITER ;
