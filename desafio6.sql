0fcfe8ef8fc0a347488780332b0ce298e84adcbb
hleoc <informig@gmail.com>
2020-10-07 19:51:34 -0300

    Item 6 - quinta verificação

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,3 @@
--- SELECT * FROM employees;
--- SELECT * FROM job_history;
--- select * from departments;
 SELECT CONCAT(E.first_name,' ', E.last_name) AS 'Nome completo',
 J.job_title AS 'Cargo',
 JH.start_date AS 'Data de início do cargo',
@@ -11,5 +8,5 @@ ON E.job_id = J.job_id
 INNER JOIN hr.departments AS D
 ON E.department_id = D.department_id
 INNER JOIN hr.job_history AS JH
-ON E.job_id = JH.job_id
+ON E.employee_id = JH.employee_id
 ORDER BY CONCAT(E.first_name,' ', E.last_name) DESC, J.job_title ASC;
