ffcaca236db05c043ef87894f7fdd92f35b43da8
Christian Duessel <duessel@gmail.com>
2020-10-07 17:15:20 -0300

    req 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+h.START_DATE AS 'Data de início do cargo', 
+e.SALARY AS 'Salário'
+FROM hr.job_history AS h
+INNER JOIN hr.employees AS e
+ON  h.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(h.START_DATE) IN(01, 02, 03)
+ORDER BY `Nome completo`, `Data de início do cargo`;	
