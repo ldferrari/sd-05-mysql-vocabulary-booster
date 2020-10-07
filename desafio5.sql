cf5ab7b7f088878bb164937919cbe8b92b716edf
hleoc <informig@gmail.com>
2020-10-07 18:15:49 -0300

    Item 5 - segunda verificação

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
-SELECT J.job_title AS 'Cargo', max_salary - min_salary AS 'Variação Salarial',
+SELECT J.job_title AS 'Cargo', J.max_salary - J.min_salary AS 'Variação Salarial',
 ROUND(AVG(J.min_salary), 2) AS 'Média mínima mensal',
 ROUND(AVG(J.max_salary), 2) AS 'Média máxima mensal'
 FROM hr.employees AS E
 INNER JOIN hr.jobs AS J
 ON E.job_id = J.job_id
 GROUP BY J.job_title
-ORDER BY max_salary - min_salary, J.job_title;
+ORDER BY J.max_salary - J.min_salary, J.job_title;
