0371eaedffda1704ae57e3297efa0baa17521526
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-07 16:10:39 -0300

    cc

--- desafio16.sql
+++ desafio16.sql
@@ -2,4 +2,3 @@
 -- Crie uma função chamada buscar_quantidade_de_empregos_por_funcionario no banco de dados hr que, ao receber o email de uma pessoa funcionária,
 --  retorne a quantidade de empregos presentes em seu histórico. Confirme que sua função retorna o valor 2 ao ser
 --  chamada passando uma pessoa funcionária cujo email é "NKOCHHAR".
-
