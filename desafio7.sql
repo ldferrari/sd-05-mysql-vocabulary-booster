46f9f8fb0eeb2f87b411584477f7533fd1404079
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-10-07 16:44:57 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,6 @@
+SELECT UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome Completo',
+JH.START_DATE AS 'Data de início', E.SALARY AS Salário
+FROM hr.employees as E
+INNER JOIN hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+WHERE MONTH(JH.START_DATE) IN ('01', '02', '03')
+ORDER BY `Nome Completo`, `Data de início`;
