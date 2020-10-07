be5a65438351a0bb2061876bd700b4e0595c9665
hleoc <informig@gmail.com>
2020-10-07 18:29:22 -0300

    Item 5 - terceira verificação

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT J.job_title AS 'Cargo', J.max_salary - J.min_salary AS 'Variação Salarial',
-ROUND(AVG(J.min_salary), 2) AS 'Média mínima mensal',
-ROUND(AVG(J.max_salary), 2) AS 'Média máxima mensal'
+ROUND(AVG(J.min_salary/12), 2) AS 'Média mínima mensal',
+ROUND(AVG(J.max_salary/12), 2) AS 'Média máxima mensal'
 FROM hr.employees AS E
 INNER JOIN hr.jobs AS J
 ON E.job_id = J.job_id
--- desafio6.sql
+++ desafio6.sql
@@ -1,2 +1,9 @@
 SELECT * FROM employees;
 SELECT * FROM jobs;
+
+SELECT CONCAT(E.first_name,' ', E.last_name) AS 'Nome completo',
+J.job_title AS 'Cargo',
+E.hire_date AS 'Data de início do cargo'
+FROM hr.employees E
+INNER JOIN hr.jobs AS J
+ON E.job_id = J.job_id;
