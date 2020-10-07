0c0931fc6e265bddaffcc8f9a6f15eebe5953831
Christian Duessel <duessel@gmail.com>
2020-10-07 20:18:19 -0300

    req7 com data de inicio sem 'do cargo'!

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
-h.START_DATE AS 'Data de início do cargo', 
+h.START_DATE AS 'Data de início', 
 e.SALARY AS 'Salário'
 FROM hr.job_history AS h
 INNER JOIN hr.employees AS e
