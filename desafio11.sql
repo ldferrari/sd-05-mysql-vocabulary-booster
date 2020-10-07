e66de265f167b8c9d338a3c043fcc8ea0121b557
ferrari <luisdiasferrari@gmail.com>
2020-10-07 14:17:39 -0300

    ferrari-mysql-vocabulary-booster

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,7 @@
 SELECT C.ContactName AS 'Nome' , C.Country AS 'País',
-	(select count(S.Country)-1
-	FROM w3schools.customers AS S
-	WHERE S.Country = C.Country) AS 'Número de compatriotas'
+(select count(S.Country)-1
+FROM w3schools.customers AS S
+WHERE S.Country = C.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS C
 HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;
-
