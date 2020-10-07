e08390b81f3c81a901d2453e442234880a044e3e
hleoc <informig@gmail.com>
2020-10-07 20:21:01 -0300

    Item 6 - sexta verificação

--- desafio6.sql
+++ desafio6.sql
@@ -3,10 +3,12 @@ J.job_title AS 'Cargo',
 JH.start_date AS 'Data de início do cargo',
 D.department_name AS 'Departamento'
 FROM hr.employees AS E
-INNER JOIN hr.jobs AS J
-ON E.job_id = J.job_id
-INNER JOIN hr.departments AS D
-ON E.department_id = D.department_id
+-- INNER JOIN hr.jobs AS J
 INNER JOIN hr.job_history AS JH
-ON E.employee_id = JH.employee_id
+ON JH.employee_id = E.employee_id
+INNER JOIN hr.departments AS D
+ON JH.department_id = D.department_id
+-- INNER JOIN hr.job_history AS JH
+INNER JOIN hr.jobs AS J
+ON J.job_id = JH.job_id
 ORDER BY CONCAT(E.first_name,' ', E.last_name) DESC, J.job_title ASC;
