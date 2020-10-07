fa6306976f997b883f7ca20e2d5dd803a844952f
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-10-07 10:05:20 -0300

    desafio 5 completo

---
+++ desafio5.sql
@@ -0,0 +1,5 @@
+SELECT JOB_TITLE as Cargo,
+MAX_SALARY - MIN_SALARY as 'Variação Salarial',
+ROUND(MIN_SALARY/12, 2) as 'Média mínima mensal',
+ROUND(MAX_SALARY/12, 2) as 'Média máxima mensal' FROM hr.jobs
+ORDER BY MAX_SALARY - MIN_SALARY, Cargo;
