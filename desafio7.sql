6132639749fec3604eae942f22cf697af1310917
Lucas Costa Ventura <lucascv333@gmail.com>
2020-10-07 13:02:53 -0300

    incluindo verificacao dos meses

--- desafio7.sql
+++ desafio7.sql
@@ -5,4 +5,5 @@ E.SALARY `Salário`
 FROM hr.employees as E
 INNER JOIN hr.job_history as JH
 ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+WHERE MONTH(JH.START_DATE) BETWEEN 1 AND 3
 ORDER BY `Nome completo`, DATE(`Data de início`);
