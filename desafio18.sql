3c8fa3f4d06cde20e7b6d3743ebf156d45e24adb
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-07 17:48:56 -0300

    cc

--- desafio18.sql
+++ desafio18.sql
@@ -10,7 +10,5 @@ FROM hr.employees
 WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE) = ano INTO hired_people_quant;
 RETURN hired_people_quant;
 END $$
-
-DELIMITER ;
- 
+DELIMITER ; 
 SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6, 1987) AS total;
