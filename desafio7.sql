82991c88c06ce3be9b3c5b9a200e6fa92c3df54d
rfreitasbatista <rfreitasbatista@gmail.com>
2020-10-07 16:11:17 -0300

    issue requirement 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT
 UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) `Nome completo`,
-JH.START_DATE `Data de início do cargo`,
+JH.START_DATE `Data de início`,
 E.SALARY Salário
 FROM hr.job_history AS JH
 INNER JOIN hr.jobs as J
