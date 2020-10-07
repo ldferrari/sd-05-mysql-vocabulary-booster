8e145b82ff0bf17fa8fefebcdaca8af650690638
Andrevlc <andrevezi@gmail.com>
2020-10-07 15:26:58 -0300

    Requisite 5 done

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,4 @@
-SELECT jobs.JOB_TITLE as Cargo, 
-ROUND(AVG(employees.salary),2) as 'Média Salarial',
-CASE WHEN ROUND(AVG(employees.salary),2) between 2000 and 5800 then 'Júnior'
-WHEN ROUND(AVG(employees.salary),2) between 5801 and 7500 then 'Pleno'
-WHEN ROUND(AVG(employees.salary),2) between 7501 and 10500 then 'Sênior'
-ELSE 'CEO'
-end as Senioridade 
-FROM employees INNER JOIN jobs ON employees.JOB_ID = jobs.JOB_ID GROUP BY `Cargo`
-ORDER BY `Média Salarial`, JOB_TITLE ASC;
+SELECT JOB_TITLE as 'Cargo', MAX_SALARY - MIN_SALARY AS `Variação Salarial`, 
+ROUND(MIN_SALARY DIV 12,2) AS `Média mínima mensal`,
+ROUND(MAX_SALARY DIV 12,2) AS `Média máxima mensal` 
+FROM jobs ORDER BY `Variação Salarial` , JOB_TITLE;
