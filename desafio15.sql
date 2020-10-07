c633a2fbc9baf25cf6fe2ce934c8ad2b38f7df9d
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-07 16:09:18 -0300

    desafio15 working but not tested

--- desafio15.sql
+++ desafio15.sql
@@ -1,9 +1,18 @@
--- Desafio 15
--- Crie uma procedure chamada buscar_media_por_cargo que recebe como parâmetro o nome de um cargo e em retorno deve mostrar a média salarial de todas as pessoas que possuem esse cargo.
---  Usando o banco hr como referência, sua procedure deve retornar somente uma coluna, com o alias "Média salarial", que mostra a média salarial arredondada para duas casas decimais.
--- 
--- Confirme a execução correta da procedure, chamando-a e passando o nome de cargo igual a "Programmer":
--- 
--- CALL buscar_media_por_cargo('Programmer');
--- 
--- Chamando-a dessa forma, sua procedure deve retornar 5760.00 como média salarial para pessoas que ocupam o cargo "Programmer".
+DROP PROCEDURE buscar_media_por_cargo;
+
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(
+IN cargo VARCHAR(100)
+)
+BEGIN 
+  SELECT
+  ROUND(AVG(SALARY), 2) AS 'Média salarial'
+  FROM hr.jobs AS j, hr.employees AS em
+  WHERE j.JOB_ID = em.JOB_ID AND
+  j.JOB_TITLE = cargo;
+END $$ 
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
