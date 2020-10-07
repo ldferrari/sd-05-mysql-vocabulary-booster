4696d3f09bf855ce9a00df13cc9d9f5b03c58b99
pedrohcalado <pedrocalado22@gmail.com>
2020-10-07 10:48:51 -0300

    fix 5 and 6

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT
 job_title AS 'Cargo',
 max_salary - min_salary AS 'Variação Salarial',
-ROUND(AVG(min_salary), 2) AS 'Média mínima mensal',
-ROUND(AVG(max_salary), 2) AS 'Média máxima mensal'
+ROUND(AVG(min_salary) / 12, 2) AS 'Média mínima mensal',
+ROUND(AVG(max_salary) / 12, 2) AS 'Média máxima mensal'
 FROM hr.jobs
 GROUP BY 1, 2
 ORDER BY 2, 1;
--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,10 @@
 SELECT
-concat(first_name, ' ', last_name) AS 'Nome completo',
+concat(employees.first_name, ' ', employees.last_name) AS `Nome completo`,
 jobs.job_title AS 'Cargo',
-employees.hire_date AS 'Data de início do cargo',
+job_history.start_date AS `Data de início do cargo`,
 departments.department_name AS 'Departamento'
 FROM hr.employees
 JOIN hr.departments ON hr.departments.department_id = hr.employees.department_id
 JOIN hr.jobs ON hr.jobs.job_id = hr.employees.job_id
-ORDER BY 1, 2;
+JOIN hr.job_history ON hr.job_history.job_id = hr.jobs.job_id
+ORDER BY `Nome completo` DESC, job_title;
