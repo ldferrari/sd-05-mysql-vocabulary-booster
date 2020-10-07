e10c0475ced60869602cac971b48f70217208bad
Christian Duessel <duessel@gmail.com>
2020-10-07 19:19:55 -0300

    req11 com -1

--- desafio11.sql
+++ desafio11.sql
@@ -3,7 +3,7 @@ A.ContactName AS 'Nome',
 A.Country AS 'País',
 (SELECT COUNT(B.Country)
 FROM w3schools.customers AS B
-WHERE A.Country = B.Country) AS 'Número de compatriotas'
+WHERE A.Country = B.Country) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS A
 HAVING `Número de compatriotas` > 0
 ORDER BY Nome;
