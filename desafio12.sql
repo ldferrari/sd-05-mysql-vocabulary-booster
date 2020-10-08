464c63e9d643fd11c0429bc6af7eda25ec3b4f9c
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-10-07 22:03:09 -0300

    alterado desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -3,7 +3,7 @@ em1.SALARY AS 'Salário funcionário 1',
 em1.PHONE_NUMBER AS 'Telefone funcionário 1',
 CONCAT(em2.FIRST_NAME, ' ', em2.LAST_NAME) AS 'Nome completo funcionário 2',
 em2.SALARY AS 'Salário funcionário 2',
-em2.PHONE_NUMBER AS 'Telefone funcionáro 2'
+em2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM hr.employees AS em1, hr.employees AS em2
 WHERE em1.JOB_ID = em2.JOB_ID
 HAVING `Nome completo funcionário 1` <> `Nome completo funcionário 2`
