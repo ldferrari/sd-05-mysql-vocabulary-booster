61aca83a8efed81af330fc5c42646d5876acb2ff
Lucas Costa Ventura <lucascv333@gmail.com>
2020-10-08 01:00:33 -0300

    trocando join por union

--- desafio14.sql
+++ desafio14.sql
@@ -1,8 +1,5 @@
-SELECT
-C.Country País
-FROM w3schools.customers AS C
-INNER JOIN w3schools.suppliers AS S
-ON C.Country = S.Country
-GROUP BY País
-ORDER BY País
+SELECT Country AS 'País' FROM w3schools.customers
+UNION
+SELECT Country AS 'País' FROM w3schools.suppliers
+ORDER BY 'País'
 LIMIT 5;
