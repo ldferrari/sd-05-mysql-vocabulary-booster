35d64fce8e593e17a9be3cbeb0dbe98d8b19caba
Preto115 <preto115@gmail.com>
2020-10-08 00:49:58 -0300

    desafio5 corrigindo bug

--- desafio5.sql
+++ desafio5.sql
@@ -5,6 +5,6 @@ ROUND((max_salary*12),2) AS "Média máxima mensal" */
 ROUND((min_salary/12),2) AS "Média mínima mensal",
 ROUND((max_salary/12),2) AS "Média máxima mensal"
 FROM hr.jobs
-GROUP BY Cargo
+-- GROUP BY Cargo
 ORDER BY `Variação Salarial`, Cargo;
 -- requisito confuso, tira-duvidas junto ao PR do Felipe Vieira
