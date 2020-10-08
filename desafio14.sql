c04cbaa247e7540774bd7bca2c7268f18d17c239
Andrevlc <andrevezi@gmail.com>
2020-10-07 21:32:56 -0300

    Requisite 14 - fixed name column

--- desafio14.sql
+++ desafio14.sql
@@ -1,7 +1,4 @@
-SELECT 
-ProductName as 'Produto',
-Price as 'Preço'
-FROM products 
-INNER JOIN order_details ON products.ProductID = order_details.ProductID
-WHERE Quantity > 80
-ORDER BY `Produto`;
+SELECT Country as "País" from customers  
+UNION 
+SELECT Country as "País" from suppliers
+ORDER BY `País` ASC LIMIT 5; 
