42dd0171c4218c6a9ded6d4ad5e885a5887cc9ce
pauloricardoz <trybe.przt@gmail.com>
2020-10-08 16:14:15 -0300

    sql 1 done

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT C.COUNTRY_NAME AS 'País', 
-IF (R.REGION_NAME = 'REGION_NAME', 'incluido', 'não incluído') AS 'Status Inclusão' 
+IF (R.REGION_NAME = 'EUROPE', 'incluído', 'não incluído') AS 'Status Inclusão' 
 FROM hr.countries as C
 JOIN hr.regions as R ON R.REGION_ID = C.REGION_ID
 ORDER BY C.COUNTRY_NAME ASC;
