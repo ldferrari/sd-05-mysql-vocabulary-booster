631d0cfba31c2c360adb25a2635b525dfafeea23
hleoc <informig@gmail.com>
2020-10-08 11:50:40 -0300

    Item 11 - primeira correção

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
-SELECT B.contactName AS 'Nome',
+SELECT A.contactName AS 'Nome',
 A.country AS 'País',
 COUNT(B.contactName) AS 'Número de compatriotas'
 FROM w3schools.customers AS A, w3schools.customers AS B
 WHERE A.customerId <> B.customerId
 AND A.country = B.country
-GROUP BY B.contactName
-ORDER BY B.contactName;
+GROUP BY A.contactName
+ORDER BY A.contactName;
