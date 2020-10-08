c8bde29e710b5f550180f7a89b5c59aa98d4d132
Mau <mauricio.sdjr@gmail.com>
2020-10-08 14:08:23 -0300

    desafio 7 e corrigindo desafio 6

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome Completo`,
+START_DATE AS `Data de início`,
+SALARY AS `Salário`
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) IN (1, 2, 3)
+ORDER BY 1, 2;
