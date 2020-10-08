8c78b434988231f70bb47ad05e42e8536b27bacf
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-10-07 21:59:27 -0300

    desafio 10 e 11 alterados

--- desafio10.sql
+++ desafio10.sql
@@ -1,5 +1,5 @@
 SELECT p.ProductName AS 'Produto',
-MIN(od.Quantity) AS 'Mínina',
+MIN(od.Quantity) AS 'Mínima',
 MAX(od.Quantity) AS 'Máxima',
 ROUND(AVG(od.Quantity), 2) AS 'Média'
 FROM w3schools.order_details AS od
--- desafio11.sql
+++ desafio11.sql
@@ -5,4 +5,5 @@ SELECT COUNT(t.Country) FROM w3schools.customers AS t
 WHERE p.Country = t.Country
 )- 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS p
+HAVING `Número de compatriotas` > 0
 Order By 1;
