8ac6ba49bbd11c5b3c4f77d3cbfb8fed3ca33f1c
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-10-08 10:51:58 -0300

    codeclimate

--- desafio16.sql
+++ desafio16.sql
@@ -4,12 +4,12 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(20))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE job_total INT;
-    SELECT COUNT(*)
-    FROM hr.job_history AS J
-    INNER JOIN hr.employees AS E ON E.EMPLOYEE_ID = J.EMPLOYEE_ID
-    WHERE E.EMAIL = email INTO job_total;
-    RETURN job_total;
+DECLARE job_total INT;
+SELECT COUNT(*)
+FROM hr.job_history AS J
+INNER JOIN hr.employees AS E ON E.EMPLOYEE_ID = J.EMPLOYEE_ID
+WHERE E.EMAIL = email INTO job_total;
+RETURN job_total;
 END $$
 
 DELIMITER ;
