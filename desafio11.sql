b2ae039021ffd7b8878bb9cbc185c7d506e2086b
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-10-07 23:23:28 -0300

    desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,4 @@
-SELECT A.ContactName AS Nome, A.Country AS País, COUNT(A.Country) - 1 AS 'Número de compatriotas'
+SELECT A.ContactName AS Nome, A.Country AS País, COUNT(A.ContactName) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS A, w3schools.customers AS B
 WHERE A.Country = B.Country
 GROUP BY A.ContactName
