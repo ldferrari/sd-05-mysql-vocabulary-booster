573dd36d054daa03829a540289583205abb018ec
hleoc <informig@gmail.com>
2020-10-07 22:07:50 -0300

    Item 7 - segunda correção

--- desafio7.sql
+++ desafio7.sql
@@ -4,4 +4,5 @@ E.salary AS 'Salário'
 FROM hr.employees AS E
 INNER JOIN hr.job_history AS JH
 ON E.employee_id = JH.employee_id
+WHERE MONTH(JH.start_date) = 01 OR MONTH(JH.start_date) = 02 OR MONTH(JH.start_date) = 03
 ORDER BY UPPER(CONCAT(E.first_name,' ', E.last_name)), JH.start_date;
