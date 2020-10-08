6e28f7374cf140f976900235bdc0a426411dd1d9
hleoc <informig@gmail.com>
2020-10-07 23:11:03 -0300

    Item 8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT C.contactName AS 'Nome de contato',
+S.shipperName AS 'Empresa que fez o envio',
+O.orderDate AS 'Data do pedido'
+FROM w3schools.customers AS C
+INNER JOIN w3schools.orders AS O
+ON C.customerId = O.customerId
+INNER JOIN w3schools.shippers AS S
+ON O.shipperId = S.shipperId
+WHERE S.shipperName = 'Speedy Express' OR S.shipperName = 'United Package'
+ORDER BY C.contactName, S.shipperName, O.orderDate;
