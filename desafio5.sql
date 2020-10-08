e8252a81f26dc592161d41592893462e9bc094b6
hleoc <informig@gmail.com>
2020-10-07 21:19:04 -0300

    Item 5 - quarta verificação

--- desafio5.sql
+++ desafio5.sql
@@ -4,5 +4,5 @@ ROUND(AVG(J.max_salary/12), 2) AS 'Média máxima mensal'
 FROM hr.employees AS E
 INNER JOIN hr.jobs AS J
 ON E.job_id = J.job_id
-GROUP BY J.job_title
+GROUP BY J.job_title, J.max_salary - J.min_salary
 ORDER BY J.max_salary - J.min_salary ASC, J.job_title ASC;
