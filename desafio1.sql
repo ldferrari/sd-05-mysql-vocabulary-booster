ce600b0fadb65c6b2b3b3751f76d0dd2cc9fc2a2
Douglas <douglaza@gmail.com>
2020-10-08 00:00:31 -0300

    desafio 1 - 2nd try

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,7 @@
-SELECT country_name AS 'País'
-FROM hr.countries
-IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+SELECT COUNTRY_NAME AS 'País', 
+IF (
+	REGION_ID=1, 'incluído',
+    'não incluído'
+) AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY País;
