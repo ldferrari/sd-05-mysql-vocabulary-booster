6120ea3deffcc50801ddb7c8f62f19f91a23bb1b
Eder Sena <eder.sena@live.com>
2020-10-08 19:48:39 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,9 @@
+SELECT jobs.JOB_TITLE as "Cargo",
+ROUND(AVG(employees.salary),2) as 'Média salarial',
+CASE WHEN ROUND(AVG(employees.salary),2) between 2000 and 5800 then 'Júnior'
+WHEN ROUND(AVG(employees.salary),2) between 5801 and 7500 then 'Pleno'
+WHEN ROUND(AVG(employees.salary),2) between 7501 and 10500 then 'Sênior'
+ELSE 'CEO'
+end as "Senioridade"
+FROM employees INNER JOIN jobs ON employees.JOB_ID = jobs.JOB_ID GROUP BY `Cargo`
+ORDER BY `Média salarial`, JOB_TITLE ASC;
