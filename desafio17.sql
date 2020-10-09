740541c4dddb02052012fcf90abb8f5931032ebe
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-09 17:00:46 -0300

    added academic honesty references

---
+++ desafio17.sql
@@ -0,0 +1,6 @@
+-- Bonus Challenge from 17 to 19, not done.
+
+-- Academic Honesty - list of consulted PR:
+-- Sidnei Ramos, https://github.com/tryber/sd-05-mysql-vocabulary-booster/pull/1
+-- Paulo Dandrea, https://github.com/tryber/sd-05-mysql-vocabulary-booster/pull/2
+-- Natalia Macedo, https://github.com/tryber/sd-05-mysql-vocabulary-booster/pull/4
