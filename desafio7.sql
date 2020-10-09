ccdd988fa94cca142c3ff29f64717f6e7c41fe60
RenataNovais <rnovaiscs@gmail.com>
2020-10-09 11:15:31 -0300

    desafio 7 - alias corrigido

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT UCASE(CONCAT(e.FIRST_NAME,' ', e.LAST_NAME)) AS 'Nome Completo',
-jh.START_DATE AS 'Data de início do cargo',
+jh.START_DATE AS 'Data de início',
 e.SALARY AS 'Salário'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
