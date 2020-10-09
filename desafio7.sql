7742c1ca7817e81dc09dbd211d96dd42bc394123
RenataNovais <rnovaiscs@gmail.com>
2020-10-09 10:12:35 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT UCASE(CONCAT(e.FIRST_NAME,' ', e.LAST_NAME)) AS 'Nome Completo',
+jh.START_DATE AS 'Data de início do cargo',
+e.SALARY AS 'Salário'
+FROM hr.employees AS e
+INNER JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(START_DATE) IN(1, 2, 3)
+ORDER BY `Nome Completo`, 2;
