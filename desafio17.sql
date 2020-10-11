a52517648fbc763f39b4d5999783f7292a7af3ba
RenataNovais <rnovaiscs@gmail.com>
2020-10-11 15:48:03 -0300

    desafio 17

--- desafio17.sql
+++ desafio17.sql
@@ -16,10 +16,11 @@
 -- Data de início - start_date from job_history -> employee_id
 -- Data de rescisão - end_date from job_history -> employee_id
 -- Anos trabalhados - end - start arredondada 2 casas
+-- HA - LPAD do PR do rafaelqleite
 
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-CONCAT(DAY(jh.START_DATE), '/', MONTH(jh.START_DATE), '/', YEAR(jh.START_DATE)) AS 'Data de ínicio',
-CONCAT(DAY(jh.END_DATE), '/', MONTH(jh.END_DATE), '/', YEAR(jh.END_DATE)) AS 'Data de rescisão',
+CONCAT(LPAD(DAY(jh.START_DATE), 2, 0), '/', LPAD(MONTH(jh.START_DATE), 2, 0), '/', YEAR(jh.START_DATE)) AS 'Data de ínicio',
+CONCAT(LPAD(DAY(jh.END_DATE), 2, 0), '/', LPAD(MONTH(jh.END_DATE), 2, 0), '/', YEAR(jh.END_DATE)) AS 'Data de rescisão',
 ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365, 2) AS 'Anos trabalhados'
 FROM hr.employees AS e
 JOIN hr.job_history AS jh
