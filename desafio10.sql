d356da357e449ce997589aef0fc86b939a433f2b
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-10-12 08:44:47 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT ProductName AS 'Produto', 
+MIN(o_d.Quantity) AS 'Mínima',
+MAX(o_d.Quantity) AS 'Máxima',
+ROUND(AVG(o_d.Quantity), 2) AS 'Média'
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS o_d ON p.ProductID = o_d.ProductID
+GROUP BY p.ProductID
+HAVING `Média` > 20
+ORDER BY Média, 'Produto';
