f89c335a8571b2bd7067486e687631be9727410d
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-10-12 12:10:42 -0300

    desafio 16 feito

---
+++ desafio16.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE total_empregos INT;
+SELECT COUNT(jh.EMPLOYEE_ID) AS 'total_empregos' FROM hr.employees AS em
+INNER JOIN hr.job_history AS jh
+ON em.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE em.EMAIL = email
+INTO total_empregos;
+RETURN total_empregos;
+END
+$$ DELIMITER ;
