309cd1185c28f131df7027ae6f0fd82ad39d64a1
Lucas Staroscky <LucasStaroscky@MacBook-Pro-de-Lucas.local>
2020-10-13 02:23:47 -0300

    corrigindo desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,16 +1,8 @@
-select * from w3schools.order_details;
-
--- ORDER DETAILS // Quantity
--- ORDERS // EmployeeID
-
 SELECT 
 CONCAT(EMP.FirstName, ' ', EMP.LastName) AS 'Nome completo',
-COUNT(ORDT.Quantity) AS 'Total de pedidos'
-FROM w3schools.employees AS EMP
-INNER JOIN w3schools.orders AS ORD
+COUNT(ORD.EmployeeID) AS 'Total de pedidos'
+FROM w3schools.orders AS ORD 
+INNER JOIN w3schools.employees AS EMP
 ON EMP.EmployeeID = ORD.EmployeeID
-INNER JOIN w3schools.order_details AS ORDT
-ON (ORDT.OrderID = ORD.OrderID)
-GROUP BY CONCAT(EMP.FirstName, ' ', EMP.LastName)
-ORDER BY COUNT(ORDT.Quantity) 
-
+GROUP BY ORD.EmployeeID
+ORDER BY 2;
