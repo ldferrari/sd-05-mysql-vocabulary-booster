80ab7746abdc2d6fb7a8380d29253c461e947abd
Marylange <marylangess@gmail.com>
2020-10-13 16:22:28 -0300

    cc

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,1 @@
-SELECT 
-	JOB_TITLE AS 'Cargo', 
-    CONCAT(MAX_SALARY, ' - ', MIN_SALARY) AS "Diferença entre salários máximo e mínimo"
-FROM hr.jobs;
+SELECT JOB_TITLE AS 'Cargo', CONCAT(MAX_SALARY, ' - ', MIN_SALARY) AS "Diferença entre salários máximo e mínimo" FROM hr.jobs;
