62f120be305092a2159a20e4040e4fb0a31415f5
Marylange <marylangess@gmail.com>
2020-10-13 17:09:38 -0300

    CC

--- desafio5.sql
+++ desafio5.sql
@@ -4,5 +4,4 @@ ROUND(min_salary/12, 2) AS "Média mínima mensal",
 ROUND(max_salary/12, 2) AS "Média máxima mensal"
 FROM hr.jobs
 ORDER BY
-max_salary - min_salary,
-job_title;
+max_salary - min_salary, job_title;
