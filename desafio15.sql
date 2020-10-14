4ed362b5637095280037f6bf45dd6efcc19a16a7
pauloricardoz <trybe.przt@gmail.com>
2020-10-14 13:05:21 -0300

    SQL 15 correcting query

--- desafio15.sql
+++ desafio15.sql
@@ -1,8 +1,10 @@
+DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo
 (IN cargo VARCHAR(50))
 BEGIN
-SELECT ROUND(AVG(SALARY),2) AS 'Média Salarial'
+SELECT ROUND(AVG(E.SALARY),2)
+AS 'Média Salarial'
 FROM hr.employees AS E
 JOIN hr.jobs AS J ON J.JOB_ID=E.JOB_ID
 WHERE J.JOB_TITLE=cargo;
