812856c784ef075d717b49814ee10a9457ce6b03
Lucas Staroscky <LucasStaroscky@MacBook-Pro-de-Lucas.local>
2020-10-14 01:00:02 -0300

    corrigindo desafio 6 e 12

--- desafio12.sql
+++ desafio12.sql
@@ -4,6 +4,6 @@ EMP.PHONE_NUMBER AS 'Telefone funcionário 1',
 CONCAT_WS(' ', SLR.FIRST_NAME, SLR.LAST_NAME) AS 'Nome completo funcionário 2',
 SLR.SALARY AS 'Salário funcionário 2',
 SLR.PHONE_NUMBER AS 'Telefone funcionário 2'
-FROM hr.EMPloyees AS EMP, hr.employees AS SLR
+FROM hr.employees AS EMP, hr.employees AS SLR
 WHERE emp.JOB_ID = SLR.JOB_ID AND emp.EMPLOYEE_ID <> SLR.EMPLOYEE_ID
 ORDER BY CONCAT_WS(' ', EMP.FIRST_NAME, EMP.LAST_NAME), CONCAT_WS(' ', SLR.FIRST_NAME, SLR.LAST_NAME);
