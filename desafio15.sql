efa9b67c1cb1642d88d2b588e32472031cb339ae
Marylange <marylangess@gmail.com>
2020-10-13 23:52:15 -0300

    CC

--- desafio15.sql
+++ desafio15.sql
@@ -1,6 +1,5 @@
 USE hr;
 DELIMITER $$
-
 CREATE PROCEDURE buscar_media_por_cargo(IN syllable VARCHAR(35))
 BEGIN
 SELECT ROUND(AVG(emp.salary), 2) AS "Média salarial"
@@ -10,7 +9,5 @@ hr.jobs AS jbs
 ON(jbs.JOB_ID = emp.JOB_ID)
 WHERE jbs.JOB_TITLE LIKE syllable;
 END $$
-
 DELIMITER ;
-
 CALL buscar_media_por_cargo('Programmer');
