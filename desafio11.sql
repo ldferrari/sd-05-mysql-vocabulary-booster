d8a642054ab6366c1795299a2c95f2391fd97de9
pauloricardoz <trybe.przt@gmail.com>
2020-10-14 10:43:21 -0300

    SQL 11 correction in query 2

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,8 @@
 SELECT C.ContactName AS 'Nome',
 C.Country AS 'País',
 COUNT(CC.Country) AS 'Número de compatriotas'
-FROM w3schools.customers AS C,w3schools.customers AS CC
+FROM w3schools.customers AS C,
+w3schools.customers AS CC
 WHERE C.Country=CC.Country AND C.ContactName<>CC.ContactName
-GROUP BY `Nome`
+GROUP BY `Nome`, `País`
 ORDER BY C.ContactName;
