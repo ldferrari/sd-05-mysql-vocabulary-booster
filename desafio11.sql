49710c1e13736573bc53b80d74251e73f8a51564
pauloricardoz <trybe.przt@gmail.com>
2020-10-14 09:44:17 -0300

    SQL 11

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT * FROM w3schools.customers;
+SELECT C.ContactName AS 'Nome',
+C.Country AS 'País',
+COUNT(CC.Country) AS 'Número de compatriotas'
+FROM w3schools.customers AS C,w3schools.customers AS CC
+WHERE C.Country=CC.Country AND C.ContactName<>CC.ContactName
+GROUP BY `Nome`
+ORDER BY C.ContactName
+;
