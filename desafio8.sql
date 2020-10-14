587f989e3047cb49f37bb435a7ee8f76a0567b76
pauloricardoz <trybe.przt@gmail.com>
2020-10-14 07:53:46 -0300

    SQL 8

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+SELECT * FROM w3schools.customers;
+SELECT * FROM w3schools.shippers;
+SELECT * FROM w3schools.orders;
+select C.ContactName as 'Nome de contato',
+S.ShipperName as 'Empresa que fez o envio',
+O.OrderDate as 'Data do pedido'
+from w3schools.customers as C
+join w3schools.orders as O ON C.CustomerID=O.CustomerID
+join w3schools.shippers as S ON S.ShipperID=O.ShipperID
+where S.ShipperName IN ('Speedy Express','United Package')
+order by `Nome de contato`, `Empresa que fez o envio`,
+`Data do pedido`;
