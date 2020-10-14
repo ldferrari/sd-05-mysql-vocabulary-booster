c4a5e0e55180d5ee4de601877c2cee810434e876
pauloricardoz <trybe.przt@gmail.com>
2020-10-14 09:26:32 -0300

    SQL 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT P.ProductName AS 'Produto',
+MIN(OD.Quantity) AS 'Mínima',
+MAX(OD.Quantity) AS 'Máxima',
+ROUND(AVG(OD.Quantity),2) AS 'Média'
+FROM w3schools.products AS P
+JOIN w3schools.order_details AS OD
+ON P.ProductID=OD.ProductID
+GROUP BY `Produto`
+having `Média`>20
+ORDER BY `Média`, `Produto`;
