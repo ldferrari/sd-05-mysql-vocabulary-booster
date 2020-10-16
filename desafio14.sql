316cefa68f2a39d3b56f200917ca98e9a646a701
Eder Sena <eder.sena@live.com>
2020-10-16 01:35:52 -0300

    cc

--- desafio14.sql
+++ desafio14.sql
@@ -1,10 +1,3 @@
---Sabemos que tanto as pessoas consumidoras quanto as empresas fornecedoras de produtos possuem países cadastrados.
---Logo, considerando o conjunto formado por essas pessoas e empresas,
---queremos saber quais são os cinco primeiros países distintos,
---em ordem alfabética, presentes nesse conjunto.
---Usando o banco w3schools como referência, monte uma query que exiba uma coluna, com o alias "País",
---que deve mostrar o nome do país.
-
 SELECT DISTINCT(grupo.Country) AS 'País'
 FROM (
   SELECT sup.Country
