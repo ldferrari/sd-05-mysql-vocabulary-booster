81ac56fa0d0f684056e284ac46dee43c08e70123
pauloricardoz <trybe.przt@gmail.com>
2020-10-19 13:49:14 -0300

    sql 15 using s lowercase

--- desafio15.sql
+++ desafio15.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE buscar_media_por_cargo
 (IN cargo VARCHAR(100))
 BEGIN
 SELECT ROUND(AVG(E.SALARY),2)
-AS 'Média Salarial'
+AS 'Média salarial'
 FROM hr.employees AS E
 JOIN hr.jobs AS J ON J.JOB_ID=E.JOB_ID
 WHERE J.JOB_TITLE=cargo;
