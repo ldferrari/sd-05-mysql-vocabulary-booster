19c1ce19f7c36423747af568bca70fc745e04e7c
luca <luucadecastro@gmail.com>
2020-10-19 02:56:06 -0300

    16

---
+++ desafio16.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE FUNCTION hr.buscar_quantidade_de_empregos_por_funcionario(emailParam VARCHAR(50))
+RETURNS INT READS SQL DATA
+Begin
+	DECLARE total_empregos INT;
+	SELECT count(*) FROM hr.employees as employee, hr.job_history as jh
+	WHERE employee.EMPLOYEE_ID = jh.EMPLOYEE_ID and employee.EMAIL = emailParam
+	INTO total_empregos;
+RETURN total_empregos;
+END $$
+
+DELIMITER ;
