75190d9b5987add63c8a29882e2cf8f485b7cf21
luialbeto <araujo.script@gmail.com>
2020-10-20 18:04:00 -0300

    correçao desafio1

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
 SELECT country_name AS 'País',
-IF(region_id = true, 'incluído', 'não incluído') AS 'Status Inclusão'
+IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 ORDER BY country_name ASC;
