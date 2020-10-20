68ea5fa436734cd58e7d166fc77fa2b88ac024df
luca <luucadecastro@gmail.com>
2020-10-20 10:05:34 -0300

    corrigindo desafio 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,7 +1,7 @@
 SELECT 
 concat(employee.first_name, ' ', employee.last_name) as 'Nome completo',
-date_format(jh.start_date, '%d/%m/%y') as 'Data de início',
-date_format(jh.end_date, '%d/%m/%y') as 'Data de rescição',
+date_format(jh.start_date, '%d/%m/%Y') as 'Data de início',
+date_format(jh.end_date, '%d/%m/%Y') as 'Data de rescisão',
 round(datediff(jh.end_date, jh.start_date) / 365, 2) as 'Anos trabalhados'
 from hr.employees as employee
 inner join job_history as jh
