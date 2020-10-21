8fb7ab99e5c3bb5b4821527424ac74195fa605da
luialbeto <araujo.script@gmail.com>
2020-10-20 22:22:00 -0300

    corrigindo 2 desafio3

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,3 @@
-SELECT Cargo, (max_salary - min_salary) AS 'Diferença entre salários máximo e mínimo'
+SELECT job_title AS Cargo, (max_salary - min_salary) AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
-ORDER BY (max_salary - min_salary) ASC, Cargo ASC;
+ORDER BY 'Diferença entre salários máximo e mínimo' ASC, 'Cargo' ASC;
