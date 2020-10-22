a4faa3e914fca6e1494c760d0acef2ae9f19edff
Larissa Palombo <larissampalombo@gmail.com>
2020-10-22 17:21:54 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome Completo',
+COUNT(od.EmployeeID) AS 'Total de pedidos'
+FROM w3schools.orders AS od
+INNER JOIN w3schools.employees AS e
+ON od.EmployeeID = e.EmployeeID
+GROUP BY od.EmployeeID
+ORDER BY 2;
