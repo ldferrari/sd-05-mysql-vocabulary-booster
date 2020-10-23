1249fb8736274ca4ee3a0c8a6a502ad5abf31572
Larissa Palombo <larissampalombo@gmail.com>
2020-10-23 16:49:35 -0300

    erro 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome Completo',
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
 COUNT(od.EmployeeID) AS 'Total de pedidos'
 FROM w3schools.orders AS od
 INNER JOIN w3schools.employees AS e
