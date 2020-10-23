c59185ff5f280e0f366ee844848584a3592b0c12
Larissa Palombo <larissampalombo@gmail.com>
2020-10-23 16:48:37 -0300

    achei o erro 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,6 @@
 SELECT p.ProductName AS 'Produto',
 MIN(o.Quantity) AS 'Mínima',
-MAS(o.Quantity) AS 'Máxima',
+MAX(o.Quantity) AS 'Máxima',
 Round(AVG(o.Quantity), 2) AS 'Média'
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS o
