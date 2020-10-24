4c64c16cefe48de44f06611908760b0fd57d62da
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-10-24 03:05:45 -0300

    desafio10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT (
+	SELECT P.ProductName
+    FROM w3schools.products AS P
+    WHERE P.ProductID = OD.ProductID
+) AS 'Produto',
+MIN(OD.Quantity) AS 'Mínima',
+MAX(OD.Quantity) AS 'Máxima',
+ROUND(AVG(OD.Quantity),2) AS 'Média'
+FROM w3schools.order_details AS OD
+GROUP BY OD.ProductID
+ORDER BY 4,1;
