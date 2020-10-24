e3099b4c8328b7e48fb0a393caeefddbc545d259
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-10-24 03:18:33 -0300

    tab que nao havia sido removido no ultimo commit

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,9 @@
 -- subquery melhor performance
 SELECT CONCAT(E.FirstName, ' ', E.LastName) AS 'Nome Completo',
 (
-	SELECT COUNT(O.EmployeeID) 
-    FROM w3schools.orders AS O
-    WHERE O.EmployeeID = E.EmployeeID
+SELECT COUNT(O.EmployeeID) 
+FROM w3schools.orders AS O
+WHERE O.EmployeeID = E.EmployeeID
 ) AS 'Total de Pedidos'
 FROM w3schools.employees AS E
 GROUP BY E.EmployeeID
