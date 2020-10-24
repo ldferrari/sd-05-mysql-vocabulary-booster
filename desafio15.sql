90590e9485eed34e130150f87d503a7cc8b66ca2
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-10-24 03:06:42 -0300

    desafio15

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
+BEGIN
+    SELECT ROUND(AVG(hre.salary),2) AS 'Média salarial'
+    FROM hr.employees AS hre
+    INNER JOIN hr.jobs AS hrj
+	ON hrj.job_id = hre.job_id
+	WHERE hrj.job_title = cargo;
+END $$
+
+DELIMITER ;
