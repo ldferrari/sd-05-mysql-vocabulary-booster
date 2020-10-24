1dcf36285b133fc8a34512febcb238435656fe2b
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-10-24 03:05:52 -0300

    desafio11

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT A.CustomerName AS 'Nome',
+A.Country AS 'País',
+(
+	SELECT COUNT(B.Country)
+	FROM w3schools.customers AS B
+    WHERE B.Country = A.Country
+) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS A
+HAVING `Número de compatriotas`
+ORDER BY Nome;
