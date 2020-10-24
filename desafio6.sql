869145e0079e4fd836d1c1749e5d99c8a42cd19b
luialbeto <araujo.script@gmail.com>
2020-10-24 20:11:27 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,18 @@
+SELECT
+CONCAT(first_name, ' ', last_name) AS 'Nome completo',
+jobs.job_title AS Cargo,
+job_history.start_date AS 'Data de início do cargo',
+departments.department_id AS 'Departamento'
+FROM hr.employees
+JOIN -- talvez eu poderia fazer um único JOIN
+hr.job_history
+ON job_history.employee_id = employees.employee_id
+JOIN
+hr.departments
+ON departments.department_id = job_history.department_id
+JOIN
+hr.jobs
+ON jobs.job_id = job_history.job_id
+ORDER BY
+`Nome completo` DESC, Cargo ASC;
+
