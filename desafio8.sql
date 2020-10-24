7d66528895d1e2f0e89faf4075614013056be089
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-10-24 03:05:33 -0300

    desafio8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT C.CustomerName AS 'Nome de contato',
+sh.ShipperName AS 'Empresa que fez o envio',
+O.OrderDate AS 'Data do pedido'
+FROM w3schools.orders AS O
+INNER JOIN w3schools.customers AS C
+ON O.CustomerID = C.CustomerID
+INNER JOIN w3schools.shippers AS sh
+ON O.ShipperID = sh.ShipperID
+WHERE sh.ShipperName IN ("Speedy Express", "United Package")
+ORDER BY 1,2,3;
