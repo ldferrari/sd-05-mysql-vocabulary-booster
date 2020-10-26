59509e52ea11cc87800f340b63d63f4317c2a8cc
luialbeto <araujo.script@gmail.com>
2020-10-26 00:03:54 -0300

    corrigindo desafio 8 (toda vez que quebro o ALIAS, cod funciona)

--- desafio8.sql
+++ desafio8.sql
@@ -4,8 +4,8 @@ shippers.shippername AS 'Empresa que fez o envio',
 orders.orderdate AS 'Data do pedido'
 FROM w3schools.customers
 INNER JOIN w3schools.orders
-ON orders.customerid = customerid
+ON orders.customerid = customers.customerid
 INNER JOIN w3schools.shippers
 ON shippers.shipperid = orders.shipperid
-WHERE `Empresa que fez o envio` = 'Speedy Express' OR `Empresa que fez o envio` = 'United Package'
-ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
+WHERE shippers.shippername = 'Speedy Express' OR shippers.shippername = 'United Package'
+ORDER BY customers.contactname, shippers.shippername, orders.orderdate;
