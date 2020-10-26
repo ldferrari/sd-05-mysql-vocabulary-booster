5a7253c2b66ac73abc6221188d5e8a06ac0e483b
Larissa Palombo <larissampalombo@gmail.com>
2020-10-26 15:56:32 -0300

    17

--- desafio17.sql
+++ desafio17.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo', DATE_FORMATE(j.START_DATE, '%d/%m/%Y') AS 'Data de início', DATE_FORMAT(j.END_DATE, '%d/%m/%Y') AS 'Data de recisão',
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo', DATE_FORMATE(j.START_DATE, '%d/%m/%Y') AS 'Data de início', DATE_FORMAT(j.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
   ROUND(DATEDIFF(j.END_DATE, j.START_DATE) / 365, 2) AS 'Anos trabalhados'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS j
