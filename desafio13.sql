4875ffdf02dc4d90500d6468835794ca3cc891fc
luialbeto <araujo.script@gmail.com>
2020-10-27 03:13:37 -0300

    corrigindo desafio 13

--- desafio13.sql
+++ desafio13.sql
@@ -4,4 +4,5 @@ p.price AS 'Preço'
 FROM w3schools.products AS p
 WHERE EXISTS 
 (SELECT * FROM w3schools.order_details
-WHERE order_details.quantity > 80 AND p.productid = order_details.productid);
+WHERE order_details.quantity > 80 AND p.productid = order_details.productid)
+ORDER BY Produto;
