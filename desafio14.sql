3e3f3972fb104a85d15adcdb108a61714c9d4d43
luialbeto <araujo.script@gmail.com>
2020-10-27 04:16:00 -0300

    corrigindo 2 desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
-SELECT suppliers.country AS País FROM w3schools.suppliers
+SELECT DISTINCT suppliers.country AS País FROM w3schools.suppliers
 UNION SELECT customers.country AS País FROM w3schools.customers
 GROUP BY País
 ORDER BY País
-LIMIT 5 OFFSET 5;
+LIMIT 5;
