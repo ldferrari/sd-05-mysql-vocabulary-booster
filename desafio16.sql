aef946409279f3f5f9b8f59c8ee949aba59872a0
luialbeto <araujo.script@gmail.com>
2020-10-27 08:40:36 -0300

    desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,18 @@
+use hr,
+
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario('presentes em seu histórico' VARCHAR(100))
+IF RETURNS = true
+{
+BEGIN
+SET hr.email= 'NKOCHHAR'
+FROM hr.employees
+END;
+}
+
+CALL buscar_quantidade_de_empregos_por_funcionario = 2
+
+DELIMITER ;
+
+-- repo vídeo https://youtu.be/WTTGml9Co90
