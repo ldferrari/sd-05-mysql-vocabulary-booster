20e167f773e5bcb4d8649b9ab048bc889ef9ac2e
luialbeto <araujo.script@gmail.com>
2020-10-27 04:11:10 -0300

    corrigindo desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
-SELECT 
-c.country AS 'País'
-FROM w3schools.customers AS c
-WHERE EXISTS(
-SELECT s.country FROM w3schools.suppliers AS s);
+SELECT suppliers.country AS País FROM w3schools.suppliers
+UNION SELECT customers.country AS País FROM w3schools.customers
+GROUP BY País
+ORDER BY País
+LIMIT 5 OFFSET 5;
