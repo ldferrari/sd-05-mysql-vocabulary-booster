657ca11485e42c90cf2d6c224f5c015310ea6b32
luialbeto <araujo.script@gmail.com>
2020-10-27 06:25:09 -0300

    corrigindo nx desafio15

--- desafio15.sql
+++ desafio15.sql
@@ -1,17 +1,17 @@
 USE hr;
 DELIMITER $$
 
-CREATE PROCEDURE buscar_media_por_cargo (IN varJob character)
+CREATE PROCEDURE buscar_media_por_cargo (IN varjob VARCHAR(100))
 BEGIN
-SELECT ROUND(AVG(employees.salary), 2) AS 'Média salarial'
+SELECT ROUND(AVG(salary), 2) AS 'Média salarial'
 FROM hr.employees
 INNER JOIN hr.jobs
-ON employees.job_id = jobs.job_id 
-WHERE jobs.job_title = varJob;
-END $$
+ON jobs.job_id = employees.job_id
+WHERE jobs.job_title = varjob;
+END
+
+CALL buscar_media_por_cargo ("Programmer");
 
 DELIMITER ;
-
-CALL buscar_media_por_cargo ('Programmer');
 
 -- query  montada com consulta a repo https://youtu.be/w3F4JQ8ndJ0
