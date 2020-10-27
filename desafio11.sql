997fbed520d6f5620bc7a091d0c0f86b349a8083
luialbeto <araujo.script@gmail.com>
2020-10-27 01:20:44 -0300

    corrigindo desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,10 @@
+-- inicialmente tentei SELF JOIN (pode ser que dê dessa forma)
 SELECT
-AS 'Nome',
-AS 'País',
-AS 'Número de compatriotas'
-FROM w3schools.contactname AS c
-ORDER BY c;
+c.contactname AS 'Nome',
+c.country AS 'País',
+COUNT(d.country) AS 'Número de compatriotas'
+FROM w3schools.customers AS c, w3schools.customers AS d
+WHERE c.customerid <> d.customerid AND c.country = d.country
+GROUP BY c.customerid
+ORDER BY c.contactname;
+-- corrigindo sintaxe (conforme evaluator job)
