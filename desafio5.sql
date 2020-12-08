8aa829c9e5e425c00136b68fba1c7c3a38b3e9c4
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-12-08 19:49:39 -0300

    adjusting decimal

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 SELECT j.JOB_TITLE AS 'Cargo',
-ROUND(j.MAX_SALARY - j.MIN_SALARY, 2) AS 'Variação Salarial',
+j.MAX_SALARY - j.MIN_SALARY AS 'Variação Salarial',
 ROUND(j.MIN_SALARY/12, 2) AS 'Média mínima mensal',
 ROUND(j.MAX_SALARY/12, 2) AS 'Média máxima mensal'
 FROM hr.jobs AS j
