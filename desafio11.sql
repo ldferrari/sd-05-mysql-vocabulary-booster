a54bb93253c3ba662d19799b1bcd828ee96889a6
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-12-09 10:52:40 -0300

    desafio 11 testing

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT c.ContactName AS 'Nome',
+c.Country AS 'País',
+COUNT(c.CustomerID) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS c
+INNER JOIN w3schools.customers AS c2
+ON c.Country = c2.Country
+GROUP BY c.CustomerID
+ORDER BY 1;
